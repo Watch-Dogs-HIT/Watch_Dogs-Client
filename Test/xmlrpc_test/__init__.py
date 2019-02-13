33d3f2dd3e51d7d964dbf7cec77af338a1724608
HouJie <h.j.13.new@gmail.com>
2019-02-13 20:08:00 +0800

    add : XMLRPC test demo

---
+++ Test/xmlrpc_test/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# encoding:utf8
+
+"""
+xmlrpclib 部分功能的测试和Demo
+"""
+
+reference_1 = "https://docs.python.org/2/library/xmlrpclib.html"
+reference_2 = "https://docs.python.org/2/library/simplexmlrpcserver.html#simplexmlrpcserver-example"
+
+# 1. 实现所有demo √
+# 2. 传递文本,字典数据 √
+# 3. 实现身份认证/主机认证/HTTPS认证功能 X
+# 4. 实现鉴权功能(?) X
+
+# 2019.2.12 改用api方式实现功能
--- Test/xmlrpc_test/xmlrpc_server.py
+++ Test/xmlrpc_test/xmlrpc_server.py
@@ -10,6 +10,16 @@ def get_a_dict():
     return {'test': 'test'}
 
 
+class A(object):
+
+    @staticmethod
+    def add(x, y):
+        return x + y
+
+    def add_1(self, x, y):
+        return x + y
+
+print A.add(2,3)
 server = SimpleXMLRPCServer(("0.0.0.0", 8081))
 print "start service get power on 0.0.0.0 8081..."
 server.register_function(get_power, "get_power")
