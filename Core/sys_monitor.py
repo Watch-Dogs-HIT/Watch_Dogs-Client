bcd6baed3a7206dfb1f1db66d078caaea180c27b
h-j-13 <h.j.13.new@gmail.com>
2020-05-24 22:21:26 +0800

    fix: compatible more /proc/diskstats format

--- Core/sys_monitor.py
+++ Core/sys_monitor.py
@@ -437,33 +437,20 @@ class SysMonitor(object):
         with open("/proc/diskstats") as f:
             lines = f.readlines()
         for line in lines:
-            # OK, this is a bit confusing. The format of /proc/diskstats can
-            # have 3 variations.
-            # On Linux 2.4 each line has always 15 fields, e.g.:
-            # "3     0   8 hda 8 8 8 8 8 8 8 8 8 8 8"
-            # On Linux 2.6+ each line *usually* has 14 fields, and the disk
-            # name is in another position, like this:
-            # "3    0   hda 8 8 8 8 8 8 8 8 8 8 8"
-            # ...unless (Linux 2.6) the line refers to a partition instead
-            # of a disk, in which case the line has less fields (7):
-            # "3    1   hda1 8 8 8 8"
-            # See:
-            # https://www.kernel.org/doc/Documentation/iostats.txt
-            # https://www.kernel.org/doc/Documentation/ABI/testing/procfs-diskstats
             fields = line.split()
-            fields_len = len(fields)
-            if fields_len == 15:
+            flen = len(fields)
+            if flen == 15:
                 # Linux 2.4
                 name = fields[3]
                 reads = int(fields[2])
                 (reads_merged, rbytes, rtime, writes, writes_merged,
-                 wbytes, wtime, _, busy_time, _) = map(int, fields[4:14])
-            elif fields_len == 14:
+                    wbytes, wtime, _, busy_time, _) = map(int, fields[4:14])
+            elif flen == 14 or flen >= 18:
                 # Linux 2.6+, line referring to a disk
                 name = fields[2]
                 (reads, reads_merged, rbytes, rtime, writes, writes_merged,
-                 wbytes, wtime, _, busy_time, _) = map(int, fields[3:14])
-            elif fields_len == 7:
+                    wbytes, wtime, _, busy_time, _) = map(int, fields[3:14])
+            elif flen == 7:
                 # Linux 2.6+, line referring to a partition
                 name = fields[2]
                 reads, rbytes, writes, wbytes = map(int, fields[3:])
